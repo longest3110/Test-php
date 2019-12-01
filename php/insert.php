4685be9799c9353557f5b6fa794eb5cb1d9299ec
Saito_Hiroki(MacBook Pro) <longest3110@gmail.com>
2019-12-01 21:54:49 +0900

    [feature/#2] クローズ処理を追加

--- php/insert.php
+++ php/insert.php
@@ -8,13 +8,15 @@
 		<?php 
 			$link = mysqli_connect('192.168.1.2', 'testsa', 'snow0order', 'TEST');
 
-			// 接続状況をチェックします
+			// 接続状況をチェック
 			if (mysqli_connect_errno()) {
 				die("データベースに接続できません:" . mysqli_connect_error() . "\n");
 			} else {
 				echo "データベースの接続に成功しました。\n";
 			}
-			
+
+			// 接続を閉じる
+			mysqli_close($link);
 		?>
 	</body>
 </html>
