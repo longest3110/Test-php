23e8b883f1b1596a7193771364e30df161be143b
Saito_Hiroki(Mac Pro) <longest3110@gmail.com>
2019-12-07 22:32:37 +0900

    [feature/#5] 内容を追加

---
+++ php/reload_test.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8">
+		<title>ReloadTest</title>
+	</head>
+	<body>
+		<form method="post" action="./reload_test.php">
+			<input type="submit" value="Reload">
+		</form>
+	</body>
+</html>
