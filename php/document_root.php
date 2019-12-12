0b314a3777288da5ad47d92ec43491409a5e3679
Saito_Hiroki(Mac Pro) <longest3110@gmail.com>
2019-12-12 23:02:11 +0900

    [feature/#6] 絶対パスでファイル読み込み

--- php/document_root.php
+++ php/document_root.php
@@ -9,6 +9,10 @@
 			$root_path = $_SERVER['DOCUMENT_ROOT'];
 			echo($root_path);
 
+			require($root_path . "/php/get_date.php");
+
 		?>
+
+		<a href="./get_date.php"><button type="button">日付</button></a><br>
 	</body>
 </html>
