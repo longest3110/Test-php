7697236c102a3fda20ea062baf3b88da90afb0ac
Saito_Hiroki(Mac Pro) <longest3110@gmail.com>
2019-12-12 22:52:28 +0900

    [feature/#6] ドキュメントルートを表示

---
+++ php/document_root.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>document_root</title>
+	</head>
+	<body>
+		<?php 
+			$root_path = $_SERVER['DOCUMENT_ROOT'];
+			echo($root_path);
+
+		?>
+	</body>
+</html>
